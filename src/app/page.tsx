f16f6ecaba1dcdc65803d727766874ca6f24fdd1
César Augusto <97255173+oCesaum@users.noreply.github.com>
2023-08-06 15:42:31 -0300

    improving design and removing unnecessary cursor pointers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="h-screen flex flex-col bg-zinc-950">
+    <div className="h-screen flex flex-col max-w-[1500px] mx-auto">
       <div className="flex flex-1 overflow-hidden">
         <Sidebar />
-        <main className="flex-1 p-6 overflow-auto rounded-lg mt-2 bg-white/5 mr-2">
+        <main className="flex-1 p-4 overflow-auto rounded-lg mt-2 bg-white/5 mr-2">
           <div className="flex items-center gap-4">
             <button className="rounded-full bg-black/40 p-1 cursor-pointer">
               <ChevronLeftIcon />
@@ -19,210 +19,183 @@ export default function Home() {
           </div>
           <section className="mt-6">
             <p className="font-semibold text-3xl">Good Afternoon</p>
-            <div className="grid grid-cols-3 gap-6 mt-4">
-              <a
-                href="#"
-                className="bg-white/5 group rounded-md flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors"
-              >
+            <div className="grid grid-cols-2 lg:grid-cols-3 gap-6 mt-4">
+              <div className="bg-white/5 group rounded-md flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors relative">
                 <Image
                   src="/eminem-the-eminem-show.jfif"
                   quality={100}
                   width={80}
                   height={80}
-                  alt="Capa do álbum Mercury - acts 1 da banda Imagine Dragons"
+                  alt="The Eminem Show"
                 />
-                <strong>Mercury</strong>
-                <button className="w-12 h-12 flex items-center justify-center pl-0.5 rounded-full bg-green-500 text-black ml-auto mr-8 invisible group-hover:visible hover:scale-105 transition-transform duration-75">
-                  <Play />
-                </button>
-              </a>
-              <a
-                href="#"
-                className="bg-white/5 group rounded-md flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors"
-              >
+                <strong>The Eminem Show</strong>
+                <div className="w-12 h-12 flex items-center justify-center pl-0.5 rounded-full bg-green-500 text-black ml-auto mr-8 invisible group-hover:visible hover:scale-105 transition-transform duration-75 absolute right-0">
+                  <Play size={24} />
+                </div>
+              </div>
+              <div className="bg-white/5 group rounded-md flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors relative">
                 <Image
                   src="/Hollywood's Bleeding.jfif"
                   quality={100}
                   width={80}
                   height={80}
-                  alt="Capa do álbum Mercury - acts 1 da banda Imagine Dragons"
+                  alt="Hollywood's Bleeding"
                 />
-                <strong>Mercury</strong>
-                <button className="w-12 h-12 flex items-center justify-center pl-0.5 rounded-full bg-green-500 text-black ml-auto mr-8 invisible group-hover:visible hover:scale-105 transition-transform duration-75">
-                  <Play />
-                </button>
-              </a>
-              <a
-                href="#"
-                className="bg-white/5 group rounded-md flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors"
-              >
+                <strong>
+                  Hollywood&apos;s Bleeding
+                </strong>
+                <div className="w-12 h-12 flex items-center justify-center pl-0.5 rounded-full bg-green-500 text-black ml-auto mr-8 invisible group-hover:visible hover:scale-105 transition-transform duration-75 absolute right-0">
+                  <Play size={24} />
+                </div>
+              </div>
+              <div className="bg-white/5 group rounded-md flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors relative">
                 <Image
                   src="/imagine dragons mercury arc 1 & 2.jfif"
                   quality={100}
                   width={80}
                   height={80}
-                  alt="Capa do álbum Mercury - acts 1 da banda Imagine Dragons"
+                  alt="Imagine Dragons Mercury arc 1 & 2"
                 />
-                <strong>Mercury</strong>
-                <button className="w-12 h-12 flex items-center justify-center pl-0.5 rounded-full bg-green-500 text-black ml-auto mr-8 invisible group-hover:visible hover:scale-105 transition-transform duration-75">
-                  <Play />
-                </button>
-              </a>
-              <a
-                href="#"
-                className="bg-white/5 group rounded-md flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors"
-              >
+                <strong>Mercury arc 1 & 2</strong>
+                <div className="w-12 h-12 flex items-center justify-center pl-0.5 rounded-full bg-green-500 text-black ml-auto mr-8 invisible group-hover:visible hover:scale-105 transition-transform duration-75 absolute right-0">
+                  <Play size={24} />
+                </div>
+              </div>
+              <div className="bg-white/5 group rounded-md flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors relative">
                 <Image
                   src="/indie gaming.jfif"
                   quality={100}
                   width={80}
                   height={80}
-                  alt="Capa do álbum Mercury - acts 1 da banda Imagine Dragons"
+                  alt="Indie Gaming"
                 />
-                <strong>Mercury</strong>
-                <button className="w-12 h-12 flex items-center justify-center pl-0.5 rounded-full bg-green-500 text-black ml-auto mr-8 invisible group-hover:visible hover:scale-105 transition-transform duration-75">
-                  <Play />
-                </button>
-              </a>
-              <a
-                href="#"
-                className="bg-white/5 group rounded-md flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors"
-              >
+                <strong>Indie Gaming</strong>
+                <div className="w-12 h-12 flex items-center justify-center pl-0.5 rounded-full bg-green-500 text-black ml-auto mr-8 invisible group-hover:visible hover:scale-105 transition-transform duration-75 absolute right-0">
+                  <Play size={24} />
+                </div>
+              </div>
+              <div className="bg-white/5 group rounded-md flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors relative">
                 <Image
                   src="/Metro Boomin Presents Spider Man Across The Spider Verse Soundtrack From And Inspired By The Motion Picture.jfif"
                   quality={100}
                   width={80}
                   height={80}
-                  alt="Capa do álbum Mercury - acts 1 da banda Imagine Dragons"
+                  alt="Metro Boomin Presents: Spider Man Across The Spider-Verse (Soundtrack From And Inspired By The Motion Picture)"
                 />
-                <strong>Mercury</strong>
-                <button className="w-12 h-12 flex items-center justify-center pl-0.5 rounded-full bg-green-500 text-black ml-auto mr-8 invisible group-hover:visible hover:scale-105 transition-transform duration-75">
-                  <Play />
-                </button>
-              </a>
-              <a
-                href="#"
-                className="bg-white/5 group rounded-md flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors"
-              >
+                <strong>
+                  Spider Man Across The Spider-Verse
+                </strong>
+                <div className="w-12 h-12 flex items-center justify-center pl-0.5 rounded-full bg-green-500 text-black ml-auto mr-8 invisible group-hover:visible hover:scale-105 transition-transform duration-75 absolute right-0">
+                  <Play size={24} />
+                </div>
+              </div>
+              <div className="bg-white/5 group rounded-md flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors relative">
                 <Image
                   src="/mix of imagine dragons.jfif"
                   quality={100}
                   width={80}
                   height={80}
-                  alt="Capa do álbum Mercury - acts 1 da banda Imagine Dragons"
+                  alt="Mix of Imagine Dragons"
                 />
-                <strong>Mercury</strong>
-                <button className="w-12 h-12 flex items-center justify-center pl-0.5 rounded-full bg-green-500 text-black ml-auto mr-8 invisible group-hover:visible hover:scale-105 transition-transform duration-75">
-                  <Play />
-                </button>
-              </a>
+                <strong>
+                  Mix of Imagine Dragons
+                </strong>
+                <div className="w-12 h-12 flex items-center justify-center pl-0.5 rounded-full bg-green-500 text-black ml-auto mr-8 invisible group-hover:visible hover:scale-105 transition-transform duration-75 absolute right-0">
+                  <Play size={24} />
+                </div>
+              </div>
             </div>
           </section>
           <section className="mt-6">
-            <p className="font-semibold text-2xl">
-              Made for César Augusto
-            </p>
-            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 2xl:grid-cols-5 gap-4 mt-4">
-              <a
-                href="#"
-                className="bg-white/5 p-4 rounded-md flex flex-col gap-2 hover:bg-white/10 group relative"
-              >
+            <p className="font-semibold text-2xl">Made for César Augusto</p>
+            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 gap-4 mt-4">
+              <div className="bg-white/5 p-4 rounded-md flex flex-col gap-2 hover:bg-white/10 group relative max-w-[220px] max-h-[300px] w-full h-full">
                 <Image
                   src="/top gaming tracks.jfif"
                   quality={100}
                   className="rounded w-full"
                   width={200}
                   height={200}
-                  alt="Capa do álbum Mercury - acts 1 da banda Imagine Dragons"
+                  alt="Top Gaming Tracks"
                 />
-                <button className="w-12 h-12 flex items-center justify-center pl-0.5 rounded-full bg-green-500 text-black invisible group-hover:visible hover:scale-105 absolute right-4 bottom-24 mr-2 mb-2 transition-transform duration-75">
-                  <Play />
-                </button>
-                <strong className="font-semibold">Daily Mix 1</strong>
+                <div className="w-12 h-12 flex items-center justify-center pl-0.5 rounded-full bg-green-500 text-black invisible group-hover:visible hover:scale-105 absolute right-4 bottom-24 mr-2 mb-2 transition-transform duration-75">
+                  <Play size={24} />
+                </div>
+                <strong className="font-semibold">DaTop Gaming Tracks</strong>
                 <span className="text-sm text-zinc-400 line-clamp-2">
-                  Imagine Dragons, Imagine Dragons and more
+                  Press play, press start.
                 </span>
-              </a>
-              <a
-                href="#"
-                className="bg-white/5 p-4 rounded-md flex flex-col gap-2 hover:bg-white/10 group relative"
-              >
+              </div>
+              <div className="bg-white/5 p-4 rounded-md flex flex-col gap-2 hover:bg-white/10 group relative max-w-[220px] max-h-[300px] w-full h-full">
                 <Image
                   src="/imagine dragons mercury arc 1.jfif"
                   quality={100}
                   className="rounded w-full"
                   width={200}
                   height={200}
-                  alt="Capa do álbum Mercury - acts 1 da banda Imagine Dragons"
+                  alt="Imagine Dragons Mercury arc 1"
                 />
-                <button className="w-12 h-12 flex items-center justify-center pl-0.5 rounded-full bg-green-500 text-black invisible group-hover:visible hover:scale-105 absolute right-4 bottom-24 mr-2 mb-2 transition-transform duration-75">
-                  <Play />
-                </button>
-                <strong className="font-semibold">Daily Mix 1</strong>
+                <div className="w-12 h-12 flex items-center justify-center pl-0.5 rounded-full bg-green-500 text-black invisible group-hover:visible hover:scale-105 absolute right-4 bottom-24 mr-2 mb-2 transition-transform duration-75">
+                  <Play size={24} />
+                </div>
+                <strong className="font-semibold">Mercury arc 1</strong>
                 <span className="text-sm text-zinc-400 line-clamp-2">
-                  Imagine Dragons, Imagine Dragons and more
+                  Imagine Dragons - Mercury arc 1.
                 </span>
-              </a>
-              <a
-                href="#"
-                className="bg-white/5 p-4 rounded-md flex flex-col gap-2 hover:bg-white/10 group relative"
-              >
+              </div>
+              <div className="bg-white/5 p-4 rounded-md flex flex-col gap-2 hover:bg-white/10 group relative max-w-[220px] max-h-[300px] w-full h-full">
                 <Image
                   src="/eminem.jfif"
                   quality={100}
-                  className="rounded w-full"
+                  className="rounded-full w-full"
                   width={200}
                   height={200}
-                  alt="Capa do álbum Mercury - acts 1 da banda Imagine Dragons"
+                  alt="Eminem"
                 />
-                <button className="w-12 h-12 flex items-center justify-center pl-0.5 rounded-full bg-green-500 text-black invisible group-hover:visible hover:scale-105 absolute right-4 bottom-24 mr-2 mb-2 transition-transform duration-75">
-                  <Play />
-                </button>
-                <strong className="font-semibold">Daily Mix 1</strong>
+                <div className="w-12 h-12 flex items-center justify-center pl-0.5 rounded-full bg-green-500 text-black invisible group-hover:visible hover:scale-105 absolute right-4 bottom-24 mr-2 mb-2 transition-transform duration-75">
+                  <Play size={24} />
+                </div>
+                <strong className="font-semibold">Eminem</strong>
                 <span className="text-sm text-zinc-400 line-clamp-2">
-                  Imagine Dragons, Imagine Dragons and more
+                  Artist
                 </span>
-              </a>
-              <a
-                href="#"
-                className="bg-white/5 p-4 rounded-md flex flex-col gap-2 hover:bg-white/10 group relative"
-              >
+              </div>
+              <div className="bg-white/5 p-4 rounded-md flex flex-col gap-2 hover:bg-white/10 group relative max-w-[220px] max-h-[300px] w-full h-full">
                 <Image
                   src="/imagine dragons mercury arc 1 & 2.jfif"
                   quality={100}
                   className="rounded w-full"
                   width={200}
                   height={200}
-                  alt="Capa do álbum Mercury - acts 1 da banda Imagine Dragons"
+                  alt="Imagine Dragons Mercury arc 1 & 2"
                 />
-                <button className="w-12 h-12 flex items-center justify-center pl-0.5 rounded-full bg-green-500 text-black invisible group-hover:visible hover:scale-105 absolute right-4 bottom-24 mr-2 mb-2 transition-transform duration-75">
-                  <Play />
-                </button>
-                <strong className="font-semibold">Daily Mix 1</strong>
+                <div className="w-12 h-12 flex items-center justify-center pl-0.5 rounded-full bg-green-500 text-black invisible group-hover:visible hover:scale-105 absolute right-4 bottom-24 mr-2 mb-2 transition-transform duration-75">
+                  <Play size={24} />
+                </div>
+                <strong className="font-semibold">Mercury arc 1 & 2</strong>
                 <span className="text-sm text-zinc-400 line-clamp-2">
-                  Imagine Dragons, Imagine Dragons and more
+                  Imagine Dragons - Mercury arc 1 & 2
                 </span>
-              </a>
-              <a
-                href="#"
-                className="bg-white/5 p-4 rounded-md flex flex-col gap-2 hover:bg-white/10 group relative"
-              >
+              </div>
+              <div className="bg-white/5 p-4 rounded-md flex flex-col gap-2 hover:bg-white/10 group relative max-w-[220px] max-h-[300px] w-full h-full">
                 <Image
                   src="/Metro Boomin Presents Spider Man Across The Spider Verse Soundtrack From And Inspired By The Motion Picture.jfif"
                   quality={100}
                   className="rounded w-full"
                   width={200}
                   height={200}
-                  alt="Capa do álbum Mercury - acts 1 da banda Imagine Dragons"
+                  alt="Metro Boomin Presents: Spider Man Across The Spider-Verse (Soundtrack From And Inspired By The Motion Picture)"
                 />
-                <button className="w-12 h-12 flex items-center justify-center pl-0.5 rounded-full bg-green-500 text-black invisible group-hover:visible hover:scale-105 absolute right-4 bottom-24 mr-2 mb-2 transition-transform duration-75">
-                  <Play />
-                </button>
-                <strong className="font-semibold">Daily Mix 1</strong>
+                <div className="w-12 h-12 flex items-center justify-center pl-0.5 rounded-full bg-green-500 text-black invisible group-hover:visible hover:scale-105 absolute right-4 bottom-24 mr-2 mb-2 transition-transform duration-75">
+                  <Play size={24} />
+                </div>
+                <strong className="font-semibold">
+                  Spider Man Across The Spider-Verse
+                </strong>
                 <span className="text-sm text-zinc-400 line-clamp-2">
-                  Imagine Dragons, Imagine Dragons and more
+                  Metro Boomin
                 </span>
-              </a>
+              </div>
             </div>
           </section>
         </main>
